eb8fcb702ddf0fca2f60e23b4256a85a05619a99
swagath shetty <udayshetty@Udays-MacBook-Air.local>
2021-04-05 20:44:47 +0530

    fixed

---
+++ src/index.js
@@ -0,0 +1,99 @@
+const app = require('express')()
+
+const { Index } = require('faunadb')
+const faunadb = require('faunadb')
+
+
+const client = new faunadb.Client({
+  secret: 'fnAEGC882-ACBQhLpRvMPI4WRhMUmqq7or4Vc2aL'
+})
+
+const {
+  Paginate,
+  Ref,
+  Get,
+  Select,
+  Match,
+  Create,
+  Collection,
+  Lambda,
+  Var,
+  Join,
+  Call,
+  Function: Fn
+} = faunadb.query
+
+
+app.get('/tweet/:id', async (req, res) => {
+
+  const doc = await client.query(
+    Get(
+      Ref(
+        Collection('tweets'),
+        req.params.id
+      )
+    )
+
+  ).catch(e => res.send(e))
+
+  res.send(doc)
+
+
+})
+
+
+app.get('/tweet', async (req, res) => {
+  const docs = await client.query(
+    Paginate(
+      Match(
+        Index('tweets_by_user'),
+        Call(Fn("getUser"), "swagath")
+      )
+    )
+  )
+})
+
+
+app.post('/tweet', async (req, res) => {
+
+  const data = {
+    user: Call(Fn("getUser"), "swagath"),
+    text: "Holma Muncho !!!!"
+  }
+
+  console.log(data)
+
+  const doc = await client.query(
+    Create(
+      Collection('tweets'),
+      { data }
+    )
+  )
+
+  console.log(doc)
+
+  res.send(doc)
+
+
+})
+
+
+app.post('/relationship', async (req, res) => {
+  const data = {
+    follower: Call(Fn("getUser"), "bob"),
+    followee: Call(Fn("getUser"), "swagath")
+  }
+
+  const doc = await client.query(
+    Create(
+      Collection('relationships'),
+      { data }
+    )
+  ).catch(e => res.send(e))
+
+  res.send(doc)
+})
+
+app.listen(5000, () => {
+  console.log('the app is working on POR 5000')
+})
